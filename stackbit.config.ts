7f1983fca98ced6c059c27e2ecd82b18185759bb
trumanharry <137094238+trumanharry@users.noreply.github.com>
2025-07-29 12:32:40 -0400

    Update stackbit.config.ts

--- stackbit.config.ts
+++ stackbit.config.ts
@@ -1,9 +1,46 @@
+// stackbit.config.ts
 import { defineStackbitConfig } from '@stackbit/types';
+import { GitContentSource } from '@stackbit/cms-git';
 
 export default defineStackbitConfig({
-    "stackbitVersion": "~0.6.0",
-    "nodeVersion": "18",
-    "ssgName": "nuxt",
-    "contentSources": [],
-    "postInstallCommand": "npm i --no-save @stackbit/types"
-})
+  stackbitVersion: '~0.6.0',
+  contentSources: [
+    new GitContentSource({
+      rootPath: __dirname,
+      contentDir: '.',
+      models: [
+        {
+          name: 'Page',
+          type: 'page',
+          urlPath: '/',
+          filePath: 'pages/index.vue',
+          fields: [
+            {
+              type: 'reference',
+              name: 'data',
+              models: ['Data'],
+            },
+          ],
+        },
+        {
+          name: 'Data',
+          type: 'data',
+          filePath: 'data/index.json',
+          fields: [
+            { type: 'object', name: 'hero', fields: [
+              { type: 'string', name: 'title', label: 'Title'},
+              { type: 'string', name: 'description', label: 'Description'},
+              { type: 'image', name: 'image', label: 'Image'},
+              { type: 'string', name: 'imageAlt', label: 'Image Alt Text'},
+              { type: 'list', name: 'buttons', items: { type: 'object', fields: [
+                { type: 'string', name: 'label', label: 'Label'},
+                { type: 'string', name: 'url', label: 'URL'}
+              ]}}
+            ]},
+            { type: 'object', name: 'testimonials', fields: [
+              { type: 'string', name: 'title', label: 'Title'},
+              { type: 'string', name: 'description', label: 'Description'},
+              { type: 'list', name: 'items', items: { type: 'object', fields: [
+                { type: 'string', name: 'quote', label: 'Quote'},
+                { type: 'object', name: 'author', fields: [
+                  { type: 'string', name: 'name', label
